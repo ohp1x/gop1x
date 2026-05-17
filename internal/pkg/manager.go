12b43fc47899d8e1d8f22c3f14ee613bde01bc8d
upiksaleh <upik@codeup.id>
2026-05-17 20:38:54 +0800

    fix: prioritize native package managers by OS
    On Linux, apt/dnf/pacman/apk are now checked before brew to prevent
    brew from being selected when both are available. On macOS, brew is
    prioritized. This fixes the issue where Ubuntu users with homebrew
    installed would have brew selected instead of apt.

--- internal/pkg/manager.go
+++ internal/pkg/manager.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"github.com/ohp1x/gop1x/internal/exec"
+	"github.com/ohp1x/gop1x/internal/platform"
 )
 
 type PackageManager interface {
@@ -27,12 +28,31 @@ func NewManagerWithRunner(r *exec.Runner) PackageManager {
 }
 
 func detectCandidates(r *exec.Runner) []PackageManager {
-	return []PackageManager{
-		&Brew{r: r},
-		&Apt{r: r},
-		&Dnf{r: r},
-		&Pacman{r: r},
-		&Apk{r: r},
-		&Nix{r: r},
+	os := platform.DetectOS()
+
+	switch os {
+	case "linux":
+		return []PackageManager{
+			&Apt{r: r},
+			&Dnf{r: r},
+			&Pacman{r: r},
+			&Apk{r: r},
+			&Nix{r: r},
+			&Brew{r: r},
+		}
+	case "darwin":
+		return []PackageManager{
+			&Brew{r: r},
+			&Nix{r: r},
+		}
+	default:
+		return []PackageManager{
+			&Brew{r: r},
+			&Apt{r: r},
+			&Dnf{r: r},
+			&Pacman{r: r},
+			&Apk{r: r},
+			&Nix{r: r},
+		}
 	}
 }
